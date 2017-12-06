8a6beb739f97308ab760d207dd4fde0bb7488db0
Keran <kerbraich@gmail.com>
2017-12-06 15:50:36 +0000

    enter players for battle

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -2,9 +2,12 @@ require './app.rb'
 
 feature "homepage content" do
 
-  scenario "should have Testing infrastructure working text" do
+  scenario "should display player names on screen" do
     visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in :player1, :with => "Player1"
+    fill_in :player2, :with => "Player2"
+    click_button "Submit"
+    expect(page).to have_text "Player1 vs. Player2"
   end
 
 end
